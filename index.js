35aa1e91a079112be5a74e73760753d867596dc5
kodirov8788 <kodirov8788@gmail.com>
2024-02-09 23:20:39 +0500

    asdas

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ const express = require('express');
 
 const app = express();
 
-
-app.get('/', (req, res) => {
-    res.send('<h1 style="border:2px solid red">Hello World</h1>');
-})
 app.get('/api', (req, res) => {
     res.send('<h1 style="border:2px solid blue">Bye World</h1>');
 })
 app.get('/api2', (req, res) => {
     res.send('<h1 style="border:2px solid green">Salom Dunyo</h1>');
+})
+app.get('/api3', (req, res) => {
+    res.send('<h1 style="border:2px solid red">Salom Dunyo</h1>');
 })
 app.listen(5001, () => {
     console.log('Server is running on port 5001');
